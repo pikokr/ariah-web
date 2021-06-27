8313caee5e9cf63d5d730348876ef69a86352c32
파랑이 <parangee9706@gmail.com>
2021-06-27 18:01:31 +0900

    feat: initial commit

---
+++ components/animatedText.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <span
+      v-for="(t, i) in text"
+      :key="i"
+      :style="{ animationDelay: i * 70 + 'ms' }"
+      class="t"
+      v-text="t"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AnimatedText',
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style scoped>
+@keyframes text-in {
+  0% {
+    transform: translateX(80px);
+    opacity: 0;
+  }
+}
+
+.t {
+  display: inline-block;
+  min-width: 0.3em;
+  animation: text-in 0.8s cubic-bezier(0.69, -0.91, 0.35, 1.94) backwards;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <h1 class="text-7xl font-black"><animated-text text="TEAM ARIAH" /></h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AnimatedText from '~/components/animatedText.vue'
+
+export default Vue.extend({
+  components: { AnimatedText },
+})
+</script>
